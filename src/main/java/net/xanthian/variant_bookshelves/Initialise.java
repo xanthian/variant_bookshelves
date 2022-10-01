6edba4f493300c21052a503ec5a42f230f874c5e
xanthian <xanneh@hotmail.com>
2022-10-01 20:06:28 +0100

    1.2.0 [Variant Parity Update] Bewitchment, BiomeMakeover, Blockus, Botania, Enriched, Paradise Lost, Promenade, Spectrum, Twigs, Wilder World

--- src/main/java/net/xanthian/variant_bookshelves/Initialise.java
+++ src/main/java/net/xanthian/variant_bookshelves/Initialise.java
@@ -16,42 +16,37 @@ import net.xanthian.variant_bookshelves.blocks.Bookshelves;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 
 public class Initialise implements ModInitializer {
 
     public static final String MOD_ID = "variant_bookshelves";
 
-    public static Identifier ID(String path) {
-        return new Identifier(MOD_ID, path);
-    }
+    public static final ItemGroup VARIANT_BOOKSHELVES =
+            FabricItemGroupBuilder.build(new Identifier(Initialise.MOD_ID, "variant_bookshelves"), () -> new ItemStack(Blocks.BOOKSHELF));
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final ItemGroup VARIANT_BOOKSHELVES = FabricItemGroupBuilder.build(new Identifier(Initialise.MOD_ID, "variant_bookshelves"),
-            () -> new ItemStack(Blocks.BOOKSHELF));
     public static List<Pair<String, String[]>> woodTypes = Lists.newArrayList();
 
     @Override
     public void onInitialize() {
 
-        Bookshelves.registerVanillaBookshelves();
+// lines commented out for duplicate materials, recipe mixin cant handle 2 items of same name :(
+
         woodTypes.add(Pair.of("acacia", new String[0]));
         woodTypes.add(Pair.of("birch", new String[0]));
         woodTypes.add(Pair.of("dark_oak", new String[0]));
         woodTypes.add(Pair.of("jungle", new String[0]));
         woodTypes.add(Pair.of("spruce", new String[0]));
+        Bookshelves.registerVanillaBookshelves();
 
         if (SharedConstants.getGameVersion().getName().startsWith("1.19")) {
             woodTypes.add(Pair.of("mangrove", new String[0]));
             Bookshelves.registerVanilla119Bookshelf();
         }
         if (!FabricLoader.getInstance().isModLoaded("betternether")) {
-            Bookshelves.registerNetherBookshelves();
             woodTypes.add(Pair.of("crimson", new String[0]));
             woodTypes.add(Pair.of("warped", new String[0]));
+            Bookshelves.registerNetherBookshelves();
         }
         if (FabricLoader.getInstance().isModLoaded("techreborn")) {
             woodTypes.add(Pair.of("rubber", new String[]{"techreborn"}));
@@ -98,6 +93,93 @@ public class Initialise implements ModInitializer {
             woodTypes.add(Pair.of("scorched", new String[]{"cinderscapes"}));
             woodTypes.add(Pair.of("umbral", new String[]{"cinderscapes"}));
             Bookshelves.registerCinderscapesBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("blockus")) {
+            woodTypes.add(Pair.of("bamboo", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("charred", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("legacy", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("white_oak", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_acacia", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_birch", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_dark_oak", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_jungle", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_mangrove", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_oak", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_spruce", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_crimson", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_warped", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_bamboo", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_charred", new String[]{"blockus"}));
+            woodTypes.add(Pair.of("herringbone_white_oak", new String[]{"blockus"}));
+            Bookshelves.registerBlockusBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("bewitchment")) {
+            //woodTypes.add(Pair.of("cypress", new String[]{"bewitchment"}));
+            woodTypes.add(Pair.of("dragons_blood", new String[]{"bewitchment"}));
+            woodTypes.add(Pair.of("elder", new String[]{"bewitchment"}));
+            woodTypes.add(Pair.of("juniper", new String[]{"bewitchment"}));
+            Bookshelves.registerBewitchmentBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("biomemakeover")) {
+            woodTypes.add(Pair.of("ancient_oak", new String[]{"biomemakeover"}));
+            woodTypes.add(Pair.of("blighted_balsa", new String[]{"biomemakeover"}));
+            woodTypes.add(Pair.of("swamp_cypress", new String[]{"biomemakeover"}));
+            //woodTypes.add(Pair.of("willow", new String[]{"biomemakeover"}));
+            Bookshelves.registerBiomeMakeoverBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("botania")) {
+            woodTypes.add(Pair.of("dreamwood", new String[]{"botania"}));
+            woodTypes.add(Pair.of("livingwood", new String[]{"botania"}));
+            woodTypes.add(Pair.of("mossy_dreamwood", new String[]{"botania"}));
+            woodTypes.add(Pair.of("mossy_livingwood", new String[]{"botania"}));
+            woodTypes.add(Pair.of("shimmerwood", new String[]{"botania"}));
+            Bookshelves.registerBotaniaBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("promenade")) {
+            woodTypes.add(Pair.of("cherry_oak", new String[]{"promenade"}));
+            woodTypes.add(Pair.of("dark_amaranth", new String[]{"promenade"}));
+            woodTypes.add(Pair.of("palm", new String[]{"promenade"}));
+            Bookshelves.registerPromenadeBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("spectrum")){
+            woodTypes.add(Pair.of("black", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("blue", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("brown", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("cyan", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("gray", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("green", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("light_blue", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("light_gray", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("lime", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("magenta", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("orange", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("pink", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("purple", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("red", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("white", new String[]{"spectrum"}));
+            woodTypes.add(Pair.of("yellow", new String[]{"spectrum"}));
+            Bookshelves.registerSpectrumBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("wilderworld")){
+            woodTypes.add(Pair.of("wisteria", new String[]{"wilderworld"}));
+            Bookshelves.registerWilderWorldBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("enriched")){
+            //woodTypes.add(Pair.of("bamboo", new String[]{"enriched"}));
+            //woodTypes.add(Pair.of("redwood", new String[]{"enriched"}));
+            Bookshelves.registerEnrichedBookshelves();
+        }
+        if (FabricLoader.getInstance().isModLoaded("twigs")){
+            woodTypes.add(Pair.of("stripped_bamboo", new String[]{"twigs"}));
+            Bookshelves.registerTwigsBookshelves();
+        }
+        if(FabricLoader.getInstance().isModLoaded("paradise_lost")) {
+            woodTypes.add(Pair.of("crystal", new String[]{"paradise_lost"}));
+            woodTypes.add(Pair.of("golden_oak", new String[]{"paradise_lost"}));
+            //woodTypes.add(Pair.of("orange", new String[]{"paradise_lost"}));
+            woodTypes.add(Pair.of("skyroot", new String[]{"paradise_lost"}));
+            //woodTypes.add(Pair.of("wisteria", new String[]{"paradise_lost"}));
+            Bookshelves.registerParadiseLostBookshelves();
         }
     }
 }
